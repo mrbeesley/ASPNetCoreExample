93af14f8faee1a2c6575707efe51268b11bbdf01
mrbeesley <mrbeesley@gmail.com>
2017-03-02 10:12:36 -0600

    Initial commit of asp net core api example

---
+++ DotNetCoreExample/src/DotNetCoreExample/Controllers/TodoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotNetCoreExample.Models;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetCoreExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            _todoItems = todoItems;
+        }
+
+        private readonly ITodoRepository _todoItems;
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+
+            return _todoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name ="GetTodo")]
+        public IActionResult GetById(long id)
+        {
+            var item = _todoItems.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+
+            _todoItems.Add(item);
+
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var todo = _todoItems.Find(id);
+            if(todo == null)
+            {
+                return NotFound();
+            }
+
+            _todoItems.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ DotNetCoreExample/src/DotNetCoreExample/Models/TodoContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreExample.Models
+{
+    public class TodoContext : IContext
+    {
+
+        public TodoContext()
+        {
+            TodoItems = new TodoContextEnumerable<TodoItem>();
+            savedTodoItems = new List<TodoItem>();
+        }
+
+        public TodoContextEnumerable<TodoItem> TodoItems { get; private set; }
+
+        private List<TodoItem> savedTodoItems;
+
+        public void SaveChanges()
+        {
+            savedTodoItems.AddRange(TodoItems.ToList());
+        }
+
+    }
+
+    public class TodoContextEnumerable<t> : IEnumerable<t>
+    {
+
+        private List<t> data = new List<t>();
+
+        public t this[int index]
+        {
+            get { return data[index]; }
+            set { data.Insert(index, value); }
+        }
+
+        public IEnumerator<t> GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public void Add(t item)
+        {
+            data.Add(item);
+        }
+
+        public void Remove(t item)
+        {
+            data.Remove(item);
+        }
+
+        public void Update(t item)
+        {
+            data.Remove(item);
+            data.Add(item);
+        }
+    }
+}
